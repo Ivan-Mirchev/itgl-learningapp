b4852f3f2695e4e8f7a23f64267983b24e9849eb
Ivan Mirchev <ivan.mirchev@hotmail.com>
2024-08-06 15:12:00 +0300

    commit

--- itgl-learningapp/obj/Debug/net8.0/itgl-learningapp.AssemblyInfo.cs
+++ itgl-learningapp/obj/Debug/net8.0/itgl-learningapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("itgl-learningapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3db9705334f373f4d1d082affa3fb8cedc3dab19")]
 [assembly: System.Reflection.AssemblyProductAttribute("itgl-learningapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("itgl-learningapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
